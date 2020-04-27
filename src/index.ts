398cc3b4486c590675810e1ae65fe05bf9609b8e
Drayton Kittel <Drayton.kittel@gmail.com>
2020-04-27 17:01:34 -0500

    mapping flow

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 const CREDENTIALS = require('../secrets/key.json');
+
+// Look here for answers : https://stackoverflow.com/questions/49236981/want-to-scrape-table-using-puppeteer-how-can-i-get-all-rows-iterate-through-ro
