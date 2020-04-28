ee82616334278c86648a5a586b8c529864660f36
Drayton Kittel <Drayton.kittel@gmail.com>
2020-04-28 16:06:49 -0500

    It's functional

--- src/URLs.ts
+++ src/URLs.ts
@@ -1,4 +1,8 @@
 export const URLs = [{
     name: "clinical 1", 
     rosterUrl: "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=bb49316f-0045-4dd1-a0db-877ae32570cf&back=INSTRUCTOR"
+},
+{
+    name: "auth - SP20",
+    rosterUrl: "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=226c4044-9f6d-4904-86c7-b5eca5b81e46&back=INSTRUCTOR"
 }]
--- src/index.ts
+++ src/index.ts
@@ -5,38 +5,82 @@ const CREDENTIALS = require('../secrets/key.json');
 
 main();
 // Look here for answers : https://stackoverflow.com/questions/49236981/want-to-scrape-table-using-puppeteer-how-can-i-get-all-rows-iterate-through-ro
+
 async function main() {
+
     for (const site of URLs) {
-        let page = await navToSite(site.rosterUrl);
-        await loginToCornerstone(page);
+
+        console.log(site.name);
+
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.setViewport({ width: 900, height: 926 });
+
+        let loginPage = await navToSite(site.rosterUrl, page);
+        let rosterPage = await loginToCornerstone(loginPage);
+        let report = await scrapeTable(rosterPage);
+        
+        console.log(report); 
+
+        browser.close();
     }
 
 }
 
-// async function getHTMLFromCrewScheduler(region: number): Promise<string> {
-//     const browser = await puppeteer.launch({ headless: true });
-//     const page = await browser.newPage();
-//     await page.setViewport({ width: 900, height: 926 });
-//     await page.goto(generalReportURL);
+async function scrapeTable(page: puppeteer.Page): Promise<string[]> {
+    // await waitUntilPageIsLoaded(page); 
+    await page.waitForSelector('table.CsListWithLines > tbody > tr')
+    let trs = await page.$$('td.Panel_contentMiddle > table.CsListWithLines > tbody > tr');
+
+    let list = [];
+
+    for (const tr of trs) {
+        let tds = await tr.$$('td');
+
+        let name = await tds[0].$eval('b', node => node.innerHTML)
+        let score = await tds[5].evaluate(node => node.innerHTML.trim())
+        let attachmentLink = await tds[8].$eval('a.action-attachment', a => a.getAttribute('href'))
 
 
-//     await page.waitForSelector('#ddlReport');
-//     await page.select('#ddlReport', '313');
+        list.push({
+            name,
+            score,
+            attachmentLink,
+            hasNewSubmission: false
+        })
 
-//     const html = await page.$eval('#DataGrid1', (element) => {
-//         return element.innerHTML
-//     })
-//     const htmlAsStirng = `'${html}'`
-//     browser.close();
-//     return htmlAsStirng;
-// }
+    }
+    let newSubmissions = [];
+    for (const student of list) {
+        if (student.score === '') {
+            //check for attachment  
+            await navToSite(student.attachmentLink, page)
+            let attachementNotFound = await page.$('#ucAttachment_upAttachments > div > span');
+            if (!attachementNotFound) {
+                student.hasNewSubmission = true;
+                newSubmissions.push(student.name);
+            }
+        }
 
-async function navToSite(url: string): Promise<puppeteer.Page> {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 900, height: 926 });
-    await page.goto(url);
-    return page;
+    }
+
+    return newSubmissions;
+}
+
+
+
+async function navToSite(url: string | null, page: puppeteer.Page): Promise<puppeteer.Page> {
+    if (url) {
+
+
+        await page.goto(url);
+        return page;
+    } else {
+        page.goto('www.google.com');
+        return page;
+    }
+
+
 
 }
 
