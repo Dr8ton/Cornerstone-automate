159f539126807ad80b29cec00ab8cefeb999945b
Drayton Kittel <Drayton.kittel@gmail.com>
2020-04-28 09:27:42 -0500

    added consts for sites

---
+++ src/URLs.ts
@@ -0,0 +1,4 @@
+export const URLs = [{
+    name: "clinical 1", 
+    rosterUrl: "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=bb49316f-0045-4dd1-a0db-877ae32570cf&back=INSTRUCTOR"
+}]
