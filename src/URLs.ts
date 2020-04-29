d56e4ee2208f430e0fcf23ef18601e1d466c15e6
Drayton Kittel <Drayton.kittel@gmail.com>
2020-04-29 16:02:51 -0500

    added Summer URLs

--- src/URLs.ts
+++ src/URLs.ts
@@ -1,6 +1,18 @@
 export const URLs = [{
-    name: "clinical 1", 
-    rosterUrl: "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=bb49316f-0045-4dd1-a0db-877ae32570cf&back=INSTRUCTOR"
+    name: "CPR - SU20", 
+    rosterUrl: "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=fa21d00f-7721-4f44-a5f7-a4aa7ed37bbf&back=INSTRUCTOR"
+},
+{
+    name: "Physical - SU20",
+    rosterUrl: "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=a9727b3e-cefe-4406-8167-f449b774de0f&back=INSTRUCTOR"
+},
+{
+    name: "Diploma - SU20",
+    rosterUrl: "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=4fbc2180-84f8-4b19-afeb-c559fd1f3603&back=INSTRUCTOR"
+},
+{
+    name: "BEMS - SU20",
+    rosterUrl: "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=d64c64a5-a89f-48fd-8d0b-4fd1d4dde710&back=INSTRUCTOR"
 },
 {
     name: "auth - SP20",
