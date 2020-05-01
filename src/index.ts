a03b70fb99343464527d0a2a5a9e92709d9e38c7
Drayton <drayton.kittel@gmail.com>
2020-04-30 23:51:43 -0500

    pagination functions

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,37 @@
 import puppeteer from 'puppeteer';
-import { URLs } from "./URLs";
+// import { URLs } from "./URLs";
 
 const CREDENTIALS = require('../secrets/key.json');
+
 
 main();
 // Look here for answers : https://stackoverflow.com/questions/49236981/want-to-scrape-table-using-puppeteer-how-can-i-get-all-rows-iterate-through-ro
 
 async function main() {
-
-    for (const site of URLs) {
+    let URLs = [
+        {
+        name: "CPR - SU20", 
+        rosterUrl: "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=fa21d00f-7721-4f44-a5f7-a4aa7ed37bbf&back=INSTRUCTOR",
+            all: [{}]
+    },
+    // {
+    //     name: "Physical - SU20",
+    //     rosterUrl: "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=a9727b3e-cefe-4406-8167-f449b774de0f&back=INSTRUCTOR"
+    // },
+    // {
+    //     name: "Diploma - SU20",
+    //     rosterUrl: "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=4fbc2180-84f8-4b19-afeb-c559fd1f3603&back=INSTRUCTOR"
+    // },
+    {
+        name: "BEMS - SU20",
+        rosterUrl: "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=d64c64a5-a89f-48fd-8d0b-4fd1d4dde710&back=INSTRUCTOR",
+    },
+    // {
+    //     name: "auth - SP20",
+    //     rosterUrl: "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=226c4044-9f6d-4904-86c7-b5eca5b81e46&back=INSTRUCTOR"
+    // }
+    ]
+    for (let site of URLs) {
 
         console.log(site.name);
 
@@ -18,74 +41,76 @@ async function main() {
 
         let loginPage = await navToSite(site.rosterUrl, page);
         let rosterPage = await loginToCornerstone(loginPage);
-        let report = await scrapeTable(rosterPage);
-
-        console.log(report);
+        let data = await scrapeTable(rosterPage);
+        site.all = data; 
 
         browser.close();
     }
-
+console.log(URLs)
 }
 
 async function scrapeTable(page: puppeteer.Page): Promise<string[]> {
-    let listOfAllStudentInTable = [];
+    let listOfAllStudentInTable: any[] = [];
+    let lastPageNumber = 2;
+    for (let index = 0; index < lastPageNumber; index++) {
 
 
+        await page.waitForSelector('table.CsListWithLines > tbody > tr')
+        let trs = await page.$$('td.Panel_contentMiddle > table.CsListWithLines > tbody > tr');
 
-    await page.waitForSelector('table.CsListWithLines > tbody > tr')
-    let trs = await page.$$('td.Panel_contentMiddle > table.CsListWithLines > tbody > tr');
+        for (const tr of trs) {
+            let tds = await tr.$$('td');
 
-    for (const tr of trs) {
-        let tds = await tr.$$('td');
+            let name = await tds[0].$eval('b', node => node.innerHTML)
+            let score = await tds[5].evaluate(node => node.innerHTML.trim())
+            let attachmentLink = await tds[8].$eval('a.action-attachment', a => a.getAttribute('href'))
 
-        let name = await tds[0].$eval('b', node => node.innerHTML)
-        let score = await tds[5].evaluate(node => node.innerHTML.trim())
-        let attachmentLink = await tds[8].$eval('a.action-attachment', a => a.getAttribute('href'))
-
-        listOfAllStudentInTable.push({
-            name,
-            score,
-            attachmentLink,
-            hasNewSubmission: false
-        })
-    }
-
-    let nextPageButton = await page.$('#ctl00_ctl00_ContentPlaceHolder1_RosterContent_pg_nextPageLink');
-    if (nextPageButton) {
-        nextPageButton.click();
-        console.log('next page button clicked')
-    }
-
-
-
-
-    let newSubmissions = [];
-    for (const student of listOfAllStudentInTable) {
-        if (student.score === '') {
-            //check for attachment  
-            await navToSite(student.attachmentLink, page)
-
-            try {
-                await page.waitForSelector('#ucAttachment_upAttachments > div > span', { timeout: 5000 })
-                console.log('element found')
-            } catch (error) {
-                console.log('element NOT found')
-                student.hasNewSubmission = true;
-                newSubmissions.push(student.name);
-            }
-
-
-            // let attachementNotFound = await page.$('#ucAttachment_upAttachments > div > span');
-
-            // if (!attachementNotFound) {
-            //     student.hasNewSubmission = true;
-            //     newSubmissions.push(student.name);
-            // }
+            listOfAllStudentInTable.push({
+                name,
+                score,
+                attachmentLink,
+                hasNewSubmission: false
+            })
         }
-
+        if (index != lastPageNumber - 1) {
+            await page.click('#ctl00_ctl00_ContentPlaceHolder1_RosterContent_pg_nextPageLink');
+        }
     }
-    console.log('newsub ', newSubmissions);
-    return newSubmissions;
+    // let nextPageButton = await page.$('#ctl00_ctl00_ContentPlaceHolder1_RosterContent_pg_nextPageLink');
+    // if (nextPageButton) {
+    //    await nextPageButton.click();
+    //     console.log('next page button clicked')
+    // }
+
+
+
+
+    // let newSubmissions = [];
+    // for (const student of listOfAllStudentInTable) {
+    //     if (student.score === '') {
+    //         //check for attachment  
+    //         await navToSite(student.attachmentLink, page)
+
+    //         try {
+    //             await page.waitForSelector('#ucAttachment_upAttachments > div > span', { timeout: 5000 })
+    //             console.log('element found')
+    //         } catch (error) {
+    //             console.log('element NOT found')
+    //             student.hasNewSubmission = true;
+    //             newSubmissions.push(student.name);
+    //         }
+
+
+    // let attachementNotFound = await page.$('#ucAttachment_upAttachments > div > span');
+
+    // if (!attachementNotFound) {
+    //     student.hasNewSubmission = true;
+    //     newSubmissions.push(student.name);
+    // }
+    // }
+
+    // }
+    return listOfAllStudentInTable;
 }
 
 
