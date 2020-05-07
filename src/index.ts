885edc385e6a530ce96fd7844c7e85852a5a4765
Drayton <drayton.kittel@gmail.com>
2020-05-06 19:38:50 -0500

    reduced urls to simple array

--- src/index.ts
+++ src/index.ts
@@ -4,38 +4,22 @@ import puppeteer from 'puppeteer';
 const CREDENTIALS = require('../secrets/key.json');
 
 interface roster {
-    name: string, 
-    rosterURL: string, 
-    data: string[]; 
+    name: string,
+    rosterURL: string,
+    data: string[];
 }
 
 main();
 // Look here for answers : https://stackoverflow.com/questions/49236981/want-to-scrape-table-using-puppeteer-how-can-i-get-all-rows-iterate-through-ro
 
 async function main() {
-    let URLs = [
-        {
-            name: "CPR - SU20",
-            rosterUrl: "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=fa21d00f-7721-4f44-a5f7-a4aa7ed37bbf&back=INSTRUCTOR",
-            data: [''] 
-        },
-        // {
-        //     name: "Physical - SU20",
-        //     rosterUrl: "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=a9727b3e-cefe-4406-8167-f449b774de0f&back=INSTRUCTOR"
-        //     ,data: [{}]
-        //     
-        // },
-        // {
-        //     name: "Diploma - SU20",
-        //     rosterUrl: "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=4fbc2180-84f8-4b19-afeb-c559fd1f3603&back=INSTRUCTOR"
-        // ,data: [{}]
 
-        // },
-        {
-            name: "BEMS - SU20",
-            rosterUrl: "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=d64c64a5-a89f-48fd-8d0b-4fd1d4dde710&back=INSTRUCTOR",
-            data: ['']
-        }
+    let URLs = [
+        "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=fa21d00f-7721-4f44-a5f7-a4aa7ed37bbf&back=INSTRUCTOR",
+        "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=a9727b3e-cefe-4406-8167-f449b774de0f&back=INSTRUCTOR",
+        "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=4fbc2180-84f8-4b19-afeb-c559fd1f3603&back=INSTRUCTOR",
+        "https://acadian.csod.com/LMS/ILT/event_session_roster.aspx?loId=d64c64a5-a89f-48fd-8d0b-4fd1d4dde710&back=INSTRUCTOR",
+
     ]
     for (let site of URLs) {
 
